cf4b4c27baaee14be777ba91d1b30aa0e677c28c
pjBooms <nikita.v.lipsky@gmail.com>
2018-10-13 22:48:54 +0700

    Jigsaw layer created that loads geecon.prague.provider module

--- src/jigsaw/layers/example/JavaConfCollector.java
+++ src/jigsaw/layers/example/JavaConfCollector.java
@@ -2,12 +2,23 @@ package jigsaw.layers.example;
 
 import jigsaw.layers.example.javaconfprovider.JavaConfProvider;
 
+import java.lang.module.ModuleFinder;
+import java.nio.file.Paths;
 import java.util.ServiceLoader;
+import java.util.Set;
 
 public class JavaConfCollector
 {
+    private static ModuleLayer createLayer(String from, String module) {
+        var finder = ModuleFinder.of(Paths.get(from));
+        var parent = ModuleLayer.boot();
+        var cf = parent.configuration().resolve(finder, ModuleFinder.of(), Set.of(module));
+        return parent.defineModulesWithOneLoader(cf, ClassLoader.getSystemClassLoader());
+    }
+
     public static void main(String[] args) {
-        var services = ServiceLoader.load(JavaConfProvider.class);
+        var layer = createLayer(args[0], "geecon.prague.provider");
+        var services = ServiceLoader.load(layer, JavaConfProvider.class);
         services.stream()
                 .map(ServiceLoader.Provider::get)
                 .forEach(confProvider ->

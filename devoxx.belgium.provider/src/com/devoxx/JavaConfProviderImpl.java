07f5f12e9b25705b1a13eacdea59a64828b9e7e2
kit <nlipsky@excelsior-usa.com>
2018-11-18 23:35:23 +0700

    Devoxx Belgium provider added

---
+++ devoxx.belgium.provider/src/com/devoxx/JavaConfProviderImpl.java
@@ -0,0 +1,11 @@
+package com.devoxx;
+
+import jigsaw.layers.example.javaconfprovider.JavaConfProvider;
+
+public class JavaConfProviderImpl implements JavaConfProvider {
+
+    @Override
+    public String provide() {
+        return "Hello from Devoxx Belgium!";
+    }
+}
